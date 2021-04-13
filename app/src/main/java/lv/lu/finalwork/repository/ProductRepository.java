08b3131632a37bf628398b9ab0ee514f15d77389
k1o <kioeddy@gmail.com>
2021-04-13 15:33:10 +0300

    add final work

--- app/src/main/java/lv/lu/finalwork/repository/ProductRepository.java
+++ app/src/main/java/lv/lu/finalwork/repository/ProductRepository.java
@@ -1,6 +1,6 @@
-package lv.lu.finalwork.finalwork.repository;
+package lv.lu.finalwork.repository;
 
-import teacher.finalwork.model.Product;
+import lv.lu.finalwork.model.Product;
 
 import java.util.List;
 import java.util.Map;
